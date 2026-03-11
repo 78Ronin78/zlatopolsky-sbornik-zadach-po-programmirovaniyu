f99fad665b80968f20aab62500e30673a03a3d24
Dmitriy Ryzhov <superdiman_1994@mail.ru>
2026-03-11 13:51:32 +0800

    build: создать C# проект задачи chapter1-3

---
+++ Chapter1/chapter1-3/csharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+  Глава 1. Простейшие программы. Арифметические выражения.
+
+  Задача 1.3. Составить программу вывода на экран числа, вводимого с клавиатуры.
+  Выводимому числу должно предшествовать сообщение "Вы ввели число".
+
+*/
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            var digit = Console.ReadLine();
+            if (int.TryParse(digit, out int d))
+            {
+                Console.WriteLine("Вы ввели целое число: {0}", digit);
+            } else
+            {
+                Console.WriteLine("Вы ввели не целое число");
+            }
+           
+        }
+    }
+}
